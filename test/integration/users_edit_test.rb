83b74f4720f2776f00431eaeb9d367c46d436d96
Daniel <kurahei.plus@gmail.com>
2019-03-28 22:54:36 +0900

    Implement authorization, friendly forwarding, index, pagination

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    t_log_in(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -17,17 +18,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', "The form contains 4 errors."
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name: "Foo Bar",
-                                              email: "foo@bar.com",
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    t_log_in(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
                                               password: "",
                                               password_confirmation: "" } }
     assert_not flash.empty?
     assert_redirected_to user_url(@user)
     @user.reload
-    assert_equal "Foo Bar", @user.name
-    assert_equal "foo@bar.com", @user.email
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
